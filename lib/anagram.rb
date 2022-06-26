1efb13d8b990c46a3fc673134af22e4cf33e51ad
muturi25 <petermuturi74@gmail.com>
2022-06-26 15:23:09 +0300

    solution to anagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words_array)
+        results = words_array.select { |words| words.split(//).sort == @word.split(//).sort }
+
+        results
+    end
+end
+
+# binding.pry
